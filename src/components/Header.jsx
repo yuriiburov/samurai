81915724544da30d63cab7770d303a177e7199f4
yuriiburov <burovyuriireact@gmail.com>
2022-03-11 23:04:00 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <header className='header box'>
+    <img src='https://t4.ftcdn.net/jpg/02/30/58/25/360_F_230582594_ds89TxI4YYbZV8POuP04YHiUKK3ZltME.jpg'/>
+  </header>
+);
+
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => (
+  <main className='content box'>
+    <div>
+      <img src='https://montegrappa.com.ua/wp-content/uploads/2019/03/Samurai-Banner.png'/>
+    </div>
+    <div>ava + desc</div>
+    <div>
+      my posts
+      <div>new post</div>
+    </div>
+    <div>
+      <div>post 1</div>
+      <div>post 2</div>
+    </div>
+  </main>
+);
+
+
+export default Profile;
